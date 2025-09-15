d761aa6bcaf46db33c3740f646facf8c4a825571
Kroese <kroese@users.noreply.github.com>
2025-09-15 17:38:44 +0200

    fix: Shellcheck warning

--- src/define.sh
+++ src/define.sh
@@ -24,7 +24,7 @@ parseVersion() {
     VERSION="${VERSION:1:-1}"
   fi
   
-  VERSION=`expr "$VERSION" : "^\ *\(.*[^ ]\)\ *$"`
+  VERSION=$(expr "$VERSION" : "^\ *\(.*[^ ]\)\ *$")
   [ -z "$VERSION" ] && VERSION="win11"
 
   local msg="is not available for ARM64 CPU's."
