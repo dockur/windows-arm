112b7e9cceda648a32a2341fb413a1778908951c
Kroese <kroese@users.noreply.github.com>
2025-09-30 10:17:09 +0200

    feat: Hide Windows Copilot button by default (#368)

--- src/define.sh
+++ src/define.sh
@@ -571,6 +571,7 @@ getMido() {
     "win11arm64" )
       size=5460387840
       sum="57d1dfb2c6690a99fe99226540333c6c97d3fd2b557a50dfe3d68c3f675ef2b0"
+      url="https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/26200.6584.250915-1905.25h2_ge_release_svc_refresh_CLIENT_CONSUMER_a64fre_en-us.iso"
       ;;
     "win11arm64-enterprise-eval" )
       size=4295096320
