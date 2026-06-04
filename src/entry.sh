8ba2344e9a9831c1be22c556396a5c76ff1e00dd
Kroese <kroese@users.noreply.github.com>
2026-06-04 20:29:00 +0200

    Enhance entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -33,23 +33,52 @@ trap - ERR
 version=$(qemu-system-aarch64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
 info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 
-if [ -z "$CPU_PIN" ]; then
-  { qemu-system-aarch64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
-else
-  { taskset -c "$CPU_PIN" qemu-system-aarch64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
+if [[ "$SHUTDOWN" != [Yy1]* ]]; then
+  if [ -z "$CPU_PIN" ]; then
+    exec qemu-system-aarch64 ${ARGS:+ $ARGS}
+  else    
+    exec taskset -c "$CPU_PIN" qemu-system-aarch64 ${ARGS:+ $ARGS}
+  fi
 fi
 
-(( rc != 0 )) && error "$(<"$QEMU_LOG")" && exit 15
+qemu() {
+  if [ -z "$CPU_PIN" ]; then
+    qemu-system-aarch64 ${ARGS:+ $ARGS} > >(
+        tee "$QEMU_PTY" | \
+        sed -u \
+        -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
+        -e 's/\x1B\x63//g' \
+        -e 's/\x1B\[[=?]7l//g' \
+        -e '/^$/d' \
+        -e 's/\x44\x53\x73//g' \
+        -e 's/failed to load Boot/skipped Boot/g' \
+        -e 's/0): Not Found/0)/g' ) &
+  else    
+    taskset -c "$CPU_PIN" qemu-system-aarch64 ${ARGS:+ $ARGS} > >(
+        tee "$QEMU_PTY" | \
+        sed -u \
+        -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
+        -e 's/\x1B\x63//g' \
+        -e 's/\x1B\[[=?]7l//g' \
+        -e '/^$/d' \
+        -e 's/\x44\x53\x73//g' \
+        -e 's/failed to load Boot/skipped Boot/g' \
+        -e 's/0): Not Found/0)/g' ) &
+  fi
+}
 
-terminal
+if [ ! -t 1 ] || [ ! -c /dev/tty ]; then
+    qemu &
+else
+    qemu </dev/tty >/dev/tty &
+fi
+
+pid=$!
 ( sleep 30; boot ) &
-tail -fn +0 "$QEMU_LOG" --pid=$$ 2>/dev/null &
-cat "$QEMU_TERM" 2> /dev/null | tee "$QEMU_PTY" | \
-sed -u -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
--e 's/\x1B\x63//g' -e 's/\x1B\[[=?]7l//g' \
--e '/^$/d' -e 's/\x44\x53\x73//g' \
--e 's/failed to load Boot/skipped Boot/g' \
--e 's/0): Not Found/0)/g' & wait $! || :
+
+rc=0
+wait "$pid" || rc=$?
+[ -f "$QEMU_END" ] && exit "$rc"
 
 sleep 1 & wait $!
-[ ! -f "$QEMU_END" ] && finish 0
+finish "$rc"
