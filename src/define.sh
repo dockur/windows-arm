c75a3c119b756673cb5febbb9725abf013cfda25
Kroese <kroese@users.noreply.github.com>
2026-08-10 14:34:20 +0200

    Update define.sh

--- src/define.sh
+++ src/define.sh
@@ -84,6 +84,14 @@ parseVersion() {
       error "Windows XP $msg" && return 1 ;;
     "2k" | "2000" | "win2k" | "win2000" | "windows2k" | "windows2000" )
       error "Windows 2000 $msg" && return 1 ;;
+    "me" | "winme" | "win9x" | "windowsme" | "windows me" )
+      error "Windows ME $msg" && return 1 ;;
+    "98" | "98se" | "win98" | "win98se" | "windows98" | "windows98se" | "windows 98" | "windows 98 se" )
+      error "Windows 98 $msg" && return 1 ;;
+    "95" | "95c" | "win95" | "win95c" | "windows95" | "windows 95" )
+      error "Windows 95 $msg" && return 1 ;;
+    "nt4" | "nt40" | "winnt4" | "windowsnt4" | "windows nt4" | "windows nt 4" )
+      error "Windows NT 4 $msg" && return 1 ;;
     "25" | "2025" | "win25" | "win2025" | "windows2025" | "windows 2025" )
       error "Windows Server 2025 $msg" && return 1 ;;
     "22" | "2022" | "win22" | "win2022" | "windows2022" | "windows 2022" )
